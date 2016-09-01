4f5718780bd7e86cb05045a751d193d9fe075c22
Alex Soto <asotobu@gmail.com>
2016-09-01 09:01:16 +0200

    adds arquillian test

---
+++ src/test/java/org/example/wildfly/HelloRestTest.java
@@ -0,0 +1,32 @@
+package org.example.wildfly;
+
+import io.restassured.RestAssured;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.wildfly.swarm.jaxrs.JAXRSArchive;
+
+import static io.restassured.RestAssured.get;
+import static org.hamcrest.CoreMatchers.is;
+
+@RunWith(Arquillian.class)
+public class HelloRestTest {
+
+    @Deployment(testable = false)
+    public static Archive createDeployment() {
+        JAXRSArchive deployment = ShrinkWrap.create(JAXRSArchive.class);
+        deployment.addResource(HelloRest.class);
+
+        return deployment;
+    }
+
+    @Test
+    public void shouldReturnHelloWorld() {
+        RestAssured.port = 8181;
+        get().then().body(is("Hello World From JavaZone 2016"));
+    }
+
+}
